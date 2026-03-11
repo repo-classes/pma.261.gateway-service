31ba2cea72df09a8a6d27d36bc7de3ec6e3b8286
Humberto Sandmann <hsandmann@ieee.org>
2026-03-11 14:12:56 -0300

    first class gateway

---
+++ src/main/java/store/gateway/GatewayResource.java
@@ -0,0 +1,20 @@
+package store.gateway;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewayResource {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Store API";
+    }
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+    
+}
